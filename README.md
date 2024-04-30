093f59db99765e523e0ae1af195748ae38b58980
Bareeth <B>
2024-04-30 20:18:51 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# apicallpackage
+
+A description of this package.
---
+++ Sources/apicallpackage/apicallpackage.swift
@@ -0,0 +1,6 @@
+public struct apicallpackage {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
